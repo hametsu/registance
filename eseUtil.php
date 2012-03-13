9fbdcb2c02061ccc331b03d150ea471de8fd1989
Akira KUMAGAI <kuma@ultrasync.net>
2012-03-14 03:06:03 +0900

    trivial

--- eseUtil.php
+++ eseUtil.php
@@ -3,19 +3,18 @@
 
 function eseFile($filename)
 {
-  $content = array();
-  try{
-    $f = fopen($filename , "r");
-    flock($f, LOCK_SH);
-    fseek($f, 0);
-    while($content[] = fgets($f));
-    flock($f, LOCK_UN);
-    fclose($f);
-    
-    unset($content[count($content)-1]);
-  }
-  catch(Exception $e){
-    return FALSE;
-  }
-  return $content;
+	$content = array();
+	try{
+		$f = fopen($filename , "r");
+		flock($f, LOCK_SH);
+		fseek($f, 0);
+		while($content[] = fgets($f));
+		flock($f, LOCK_UN);
+		fclose($f);
+		
+		unset($content[count($content)-1]);
+	}catch(Exception $e){
+		$content = FALSE;
+	}
+	return $content;
 }
